c6889c2b70ff091fd25758da2010dbaba201d9a5
matt patt <matt-patt@users.noreply.github.com>
2015-05-15 19:01:43 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 var deg = 0;
 window.setInterval(function () {
-      document.body.style.transform="rotate(" + deg + "deg)" ;
+      document.getElementById("main").style.transform="rotate(" + deg + "deg)" ;
       deg+=1;
     }, 50);
